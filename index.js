94e6d3cb2c89718e79a891a05efded83a3d38d67
rayhankhan <khanrayhanali@gmail.com>
2022-10-06 15:32:30 -0500

    Update 7 - Added wub conditions + functionality, changed UI colors, added replay button

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-// const gameState = {
-//     players: ['x', 'o'],
-//     board: [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
-
-
-const winningConditionHorizontal = [
-    [0, 1, 2]
-    [3 ,4 ,5]
-    [6 ,7 ,8]
+var baseBoard;
+const player1 = 'X';
+const player2 = 'O';
+const winningConditions = [
+    [0, 1, 2],
+    [3 ,4 ,5],
+    [6 ,7 ,8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
+const cells = document.querySelectorAll(".cell");
+startGame();
 
+function startGame() {
+    document.querySelector(".gameover").getElementsByClassName.display = "none";
+    baseBoard = Array.from(Array(9).keys());
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
 
+function turnClick(square) {
+    turn(square.target.id, player1)
+}
 
+function turn(squareId, player) {
+    baseBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWin = checkWin(baseBoard, player)
+    if (gameWin) gameOver(gameWin)
+}
 
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, []);
+    let gameWin = null;
+    for (let [index, win] of winningConditions.entries()) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWin = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWin;
+}
 
-
-
-
-
-
-
-
-
-
-// state: {
-//     initial: 'menu',
-//     events: [
-
-//     ]
-// }
-
-// const playerOneColor = document.getElementById("one");
-// const playerTwoColor = document.getElementById("two");
-
-// let nameChange1 = 'red';
-// let nameChange2 = 'blue';
-
-
-// playerOneColors.addEventListener("change", function(event) {
-//     chosenColor = event.target.value
-// });
-
-// table.onclick = function colorize(event){
-//     if (event.target.tagName === "TD") {
-//         const target = event.target
-//         if (target.className.length) {
-//             target.className = ''
-//         } else {
-//             target.className = chosenColor
-//         }
-//     }
-// }
+function gameOver(gameWin) {
+    for (let index of winningConditions[gameWin.index]) {
+        document.getElementById(index).style.backgroundColor = gameWin.player == player1 ? "#6ee2ffff" : "#ff410dff";
+    }
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+}
