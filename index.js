135f22d0e113337757283d8cc1a2d49b45203dbd
rayhankhan <khanrayhanali@gmail.com>
2022-10-06 13:30:09 -0500

    Update 6 - fixed board layout

--- index.js
+++ index.js
@@ -7,3 +7,52 @@
 //     ]
 // }
 
+
+const winningConditionHorizontal = [
+    [0, 1, 2]
+    [3 ,4 ,5]
+    [6 ,7 ,8]
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// state: {
+//     initial: 'menu',
+//     events: [
+
+//     ]
+// }
+
+// const playerOneColor = document.getElementById("one");
+// const playerTwoColor = document.getElementById("two");
+
+// let nameChange1 = 'red';
+// let nameChange2 = 'blue';
+
+
+// playerOneColors.addEventListener("change", function(event) {
+//     chosenColor = event.target.value
+// });
+
+// table.onclick = function colorize(event){
+//     if (event.target.tagName === "TD") {
+//         const target = event.target
+//         if (target.className.length) {
+//             target.className = ''
+//         } else {
+//             target.className = chosenColor
+//         }
+//     }
+// }
