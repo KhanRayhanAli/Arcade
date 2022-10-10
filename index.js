9dd6d13ff4853e19d81eba7421b861cc64049cce
rayhankhan <khanrayhanali@gmail.com>
2022-10-09 21:33:18 -0500

    Menu functionality

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var baseBoard;
-const player1 = 'X';
-const player2 = 'O';
+let baseBoard;
+const player = {
+    "one": "X",
+    "two": "O",
+    "computer": "O"
+}
+let opponent;
+const setBox = document.querySelector(".setBox");
+const options = setBox.querySelector(".playOptions")
+const computerBtn = setBox.querySelector(".computer");
+const friendBtn = setBox.querySelector(".friend");
+const playBtn = setBox.querySelector(".play");
+const xName = setBox.querySelector(".nameInput1")
+const oName = setBox.querySelector(".nameInput2")
+
 const winningConditions = [
     [0, 1, 2],
     [3 ,4 ,5],
@@ -11,6 +23,43 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+
+
+function playComputer() {
+    opponent = player.computer;
+    hideMenu(options)
+    hideMenu(computerBtn)
+    hideMenu(friendBtn)
+    showMenu(xName)
+}
+
+function playFriend() {
+    opponent = player.two;
+    hideMenu(options)
+    hideMenu(computerBtn)
+    hideMenu(friendBtn)
+    showMenu(xName)
+    showMenu(oName)
+    showMenu(playBtn)
+}
+
+function hideMenu(hidden) {
+    hidden.classList.add("hide")
+}
+
+function showMenu(shown) {
+    shown.classList.remove("hide")
+}
+
+function setNameX(){
+    let player1Name = document.getElementById('player1').value;
+    document.getElementById('playerX').innerText = player1Name;
+}
+
+function setNameO(){
+    let player2Name = document.getElementById('player2').value;
+    document.getElementById('playerO').innerText = player2Name;
+}
 
 const cells = document.querySelectorAll(".cell");
 startGame();
@@ -26,8 +75,14 @@ function startGame() {
 }
 
 function turnClick(square) {
-    turn(square.target.id, player1)
+        turn(square.target.id, player1)
 }
+
+// function turnSwitch(square) {
+//     if (turn(square.target.id, player1)){
+//         turn(square.target.id, player2)
+//     }
+// }
 
 function turn(squareId, player) {
     baseBoard[squareId] = player;
