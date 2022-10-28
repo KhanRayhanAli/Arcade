dfa5fbe00a84ed07341bf3df540c600bf9336018
rayhankhan <khanrayhanali@gmail.com>
2022-10-27 22:59:01 -0500

    Player swap functionality, turn indicator, Single input per square

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let baseBoard;
 const player = {
-    "one": "X",
-    "two": "O",
-    "computer": "O"
+  one: "X",
+  two: "O",
+  comp: "O",
 };
-const currentPlayer = '';
-let opponent;
+
+let currentPlayer;
+let opponent = "";
 const setBox = document.querySelector(".setBox");
-const options = setBox.querySelector(".playOptions")
+const options = setBox.querySelector(".playOptions");
 const computerBtn = setBox.querySelector(".computer");
 const friendBtn = setBox.querySelector(".friend");
 const playBtn = setBox.querySelector(".play");
@@ -15,6 +16,7 @@ const backBtn = setBox.querySelector(".back");
 
 const xName = setBox.querySelector(".nameInput1");
 const oName = setBox.querySelector(".nameInput2");
+let currentName = document.querySelector("#currentPlayerName").innerText;
 
 const head = document.querySelector(".head");
 const game = document.querySelector(".game");
@@ -22,141 +24,170 @@ const gameFinish = document.querySelector(".gameover");
 const cells = document.querySelectorAll(".cell");
 
 const winningConditions = [
-    [0, 1, 2],
-    [3 ,4 ,5],
-    [6 ,7 ,8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-function playComputer() {
-    opponent = player.computer;
-    computerO();
-    hideMenu(options);
-    hideMenu(computerBtn);
-    hideMenu(friendBtn);
-    showMenu(xName);
-    showMenu(backBtn);
-    showMenu(playBtn);
-}
-
-function playFriend() {
-    opponent = player.two;
-    hideMenu(options);
-    hideMenu(computerBtn);
-    hideMenu(friendBtn);
-    showMenu(xName);
-    showMenu(oName);
-    showMenu(backBtn);
-    showMenu(playBtn);
-    
-}
-
-function backTrack() {
-    hideMenu(xName);
-    hideMenu(oName);
-    hideMenu(backBtn);
-    hideMenu(playBtn);
-    showMenu(options);
-    showMenu(computerBtn);
-    showMenu(friendBtn);
-}
-
-function playGame() {
-    // if (playerX == null) {
-    //     alert('Enter a name!')
-    // } else {
-        hideMenu(setBox);
-        showMenu(head);
-        showMenu(game);
-    // }
-}
-
-function backStart() {
-    hideMenu(options);
-    hideMenu(head);
-    hideMenu(game);
-    hideMenu(xName);
-    hideMenu(oName);
-    hideMenu(backBtn);
-    hideMenu(playBtn);
-    showMenu(computerBtn);
-    showMenu(friendBtn);
-    showMenu(setBox);
-    
-}
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function hideMenu(hidden) {
-    hidden.classList.add("hide")
+  hidden.classList.add("hide");
 }
 
 function showMenu(shown) {
-    shown.classList.remove("hide")
+  shown.classList.remove("hide");
 }
 
-function setNameX(){
-    let player1Name = document.getElementById('player1').value;
-    document.getElementById('playerX').innerText = player1Name;
-    
+function setNameX() {
+  let player1Name = document.getElementById("player1").value;
+  document.getElementById("playerX").innerText = player1Name;
 }
 
-function setNameO(){
-    let player2Name = document.getElementById('player2').value;
-    document.getElementById('playerO').innerText = player2Name;
+function setNameO() {
+  let player2Name = document.getElementById("player2").value;
+  document.getElementById("playerO").innerText = player2Name;
 }
 
-function computerO(){
-    document.getElementById('playerO').innerText = 'Computer'
+function computerO() {
+  opponent = player.comp;
+  document.getElementById("playerO").innerText = "Computer";
+}
+
+function playerTwoO() {
+  opponent = player.two;
+}
+
+function playComputer() {
+  computerO();
+  hideMenu(options);
+  hideMenu(computerBtn);
+  hideMenu(friendBtn);
+  showMenu(xName);
+  showMenu(backBtn);
+  showMenu(playBtn);
+}
+
+function playFriend() {
+  playerTwoO();
+  hideMenu(options);
+  hideMenu(computerBtn);
+  hideMenu(friendBtn);
+  showMenu(xName);
+  showMenu(oName);
+  showMenu(backBtn);
+  showMenu(playBtn);
+}
+
+function backTrack() {
+  hideMenu(xName);
+  hideMenu(oName);
+  hideMenu(backBtn);
+  hideMenu(playBtn);
+  showMenu(options);
+  showMenu(computerBtn);
+  showMenu(friendBtn);
+}
+
+function playGame() {
+  hideMenu(setBox);
+  showMenu(head);
+  showMenu(game);
+  const currentPlayers = [player.one, opponent];
+  currentPlayer =
+    currentPlayers[Math.floor(Math.random() * currentPlayers.length)];
+    turnIndicator();
+}
+
+function backStart() {
+  hideMenu(options);
+  hideMenu(head);
+  hideMenu(game);
+  hideMenu(xName);
+  hideMenu(oName);
+  hideMenu(backBtn);
+  hideMenu(playBtn);
+  hideMenu(gameFinish);
+  showMenu(computerBtn);
+  showMenu(friendBtn);
+  showMenu(setBox);
+  showMenu(options);
+  baseBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
 }
 
 startGame();
 
 function startGame() {
-    hideMenu(gameFinish)
-    baseBoard = Array.from(Array(9).keys());
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
-    }
+  hideMenu(gameFinish);
+  baseBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  };
 }
 
 function turnClick(square) {
-        turn(square.target.id, player.one)
+  turn(square.target.id, currentPlayer);
+  swapPlayer();
+  cells[square.target.id].removeEventListener("click", turnClick, false);
 }
+
+function swapPlayer() {
+  if (currentPlayer === player.one) {
+    currentPlayer = opponent;
+  } else currentPlayer = player.one;
+  turnIndicator();
+}
+
+function turnIndicator() {
+  if (currentPlayer === opponent) {
+      document.getElementById("currentPlayerName").innerText =
+        document.getElementById("playerO").innerText;
+  } else {
+    document.getElementById("currentPlayerName").innerText =
+      document.getElementById("playerX").innerText;
+  }
+};
 
 function turn(squareId, player) {
-    baseBoard[squareId] = player;
-    document.getElementById(squareId).innerText = player;
-    let gameWin = checkWin(baseBoard, player)
-    if (gameWin) gameOver(gameWin)
-}
+  baseBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let gameWin = checkWin(baseBoard, player);
+  if (gameWin) gameOver(gameWin);
+};
 
 function checkWin(board, player) {
-    let plays = board.reduce((a, e, i) =>
-        (e === player) ? a.concat(i) : a, []);
-    let gameWin = null;
-    for (let [index, win] of winningConditions.entries()) {
-        if (win.every(elem => plays.indexOf(elem) > -1)) {
-            gameWin = {index: index, player: player};
-            break;
-        }
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWin = null;
+  for (let [index, win] of winningConditions.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWin = { index: index, player: player };
+      break;
     }
-    return gameWin;
-}
+  }
+  return gameWin;
+};
 
 function gameOver(gameWin) {
-    for (let index of winningConditions[gameWin.index]) {
-        document.getElementById(index).style.backgroundColor = gameWin.player == player.one ? "#6ee2ffff" : "#ff410dff";
-    }
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', turnClick, false);
-    }
-    showMenu(gameFinish)
-    if (gameWin.player == player.one) {
-        document.getElementsByClassName(".winner").innerText = document.getElementById('player1').value + ' Wins!'
-    }
-}
+  for (let index of winningConditions[gameWin.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWin.player == player.one ? "#6ee2ffff" : "#ff410dff";
+  }
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
+  showMenu(gameFinish);
+  if (gameWin.player === player.one) {
+    document.getElementsByClassName(".winner").innerText =
+      document.getElementById("playerX").innerText + " Wins!";
+  }
+};
